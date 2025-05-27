cf22d56abe5f9e89f36e7841cd2b2572cffd43c6
yanyijue <3326288718@qq.com>
2025-05-27 22:14:34 +0800

    test one

--- test5.7.py
+++ test5.7.py
@@ -23,21 +23,21 @@
 # plt.show()
 
 
-from sklearn.datasets import load_wine
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-wine = load_wine ()
-X =wine.data
-scaler = StandardScaler()
-X =scaler.fit_transform(X)
-kmeans = KMeans(n_clusters=3, random_state=20,n_init=10)
-kmeans.fit(X)
-labels = kmeans.labels_
-centroids = kmeans.cluster_centers_
-plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=200, color='red')
-plt.scatter(X[:,0],X[:,1],c=labels, s=10)
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title('Kmeans(Sklearn)')
-plt.show()
+# from sklearn.datasets import load_wine
+# from sklearn.cluster import KMeans
+# from sklearn.preprocessing import StandardScaler
+# import matplotlib.pyplot as plt
+# wine = load_wine ()
+# X =wine.data
+# scaler = StandardScaler()
+# X =scaler.fit_transform(X)
+# kmeans = KMeans(n_clusters=3, random_state=20,n_init=10)
+# kmeans.fit(X)
+# labels = kmeans.labels_
+# centroids = kmeans.cluster_centers_
+# plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=200, color='red')
+# plt.scatter(X[:,0],X[:,1],c=labels, s=10)
+# plt.xlabel('Feature 1')
+# plt.ylabel('Feature 2')
+# plt.title('Kmeans(Sklearn)')
+# plt.show()
